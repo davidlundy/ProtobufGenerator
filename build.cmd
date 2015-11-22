a06b6a4bbb4c8ab5262c16936dcfa09630351426
David Lundy <draylundy@gmail.com>
2015-11-22 20:26:04 +0200

    Try again to generate Coveralls report
    Environment variables aren't files. Can't EXIST them.

--- build.cmd
+++ build.cmd
@@ -38,6 +38,6 @@ IF NOT EXIST packages\coveralls.net (
 :run
 packages\Sake\tools\Sake.exe -f makefile.shade %*
 
-IF EXIST %APPVEYOR% (
+IF %APPVEYOR% == true(
 	packages\coveralls.net\tools\csmacnz.Coveralls.exe --opencover -i ./artifacts/reports/coverage.xml --repoToken %COVERALLS_REPO_TOKEN% --commitId %APPVEYOR_REPO_COMMIT% --commitBranch %APPVEYOR_REPO_BRANCH% --commitAuthor %APPVEYOR_REPO_COMMIT_AUTHOR% --commitEmail %APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL% --commitMessage %APPVEYOR_REPO_COMMIT_MESSAGE% --jobId %APPVEYOR_JOB_ID%
 )
